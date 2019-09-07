54a8eae95a0c59624c2b6f1a094cfe1297e447b7
juliastetskaya <juliastetskaya@gmail.com>
2019-09-07 12:24:59 +0300

    project skeleton

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,5 +1,16 @@
 export default class BookstoreService {
   getBooks() {
-    return [];
+    return [
+      {
+        id: 1,
+        title: 'The 7 Habits of Highly Effective People',
+        author: 'Stephen R. Covey',
+      },
+      {
+        id: 2,
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+      }
+    ];
   }
 }
