b771cc037361a51b59fdb126645b5779fbe90d2e
juliastetskaya <juliastetskaya@gmail.com>
2019-09-09 08:26:29 +0300

    add book-list styles

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,19 +3,21 @@ import { Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from '../pages';
 
 const App = () => (
-  <div className="container">
-    <Switch>
-      <Route
-        path="/"
-        component={HomePage}
-        exact
-      />
-      <Route
-        path="/cart"
-        component={CartPage}
-      />
-    </Switch>
-  </div>
+  <main role="main" className="container">
+    <div className="container">
+      <Switch>
+        <Route
+          path="/"
+          component={HomePage}
+          exact
+        />
+        <Route
+          path="/cart"
+          component={CartPage}
+        />
+      </Switch>
+    </div>
+  </main>
 );
 
 export default App;
--- src/components/book-list-item/book-list-item.jsx
+++ src/components/book-list-item/book-list-item.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 const BookListItem = ({ book }) => {
-  const { title, author } = book;
+  const {
+    title, author, price, coverImage,
+  } = book;
 
   return (
-    <>
-      <span>{title}</span>
-      <span>{author}</span>
-    </>
+    <div className="book-list-item">
+      <div className="book-cover">
+        <img src={coverImage} alt="cover" />
+      </div>
+      <div className="book-details">
+        <a href="#1" className="book-title">{title}</a>
+        <div className="book-author">{author}</div>
+        <div className="book-price">
+          $
+          {price}
+        </div>
+        <button className="btn btn-info add-to-cart" type="button">Add to cart</button>
+      </div>
+    </div>
   );
 };
 
