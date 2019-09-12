8d11159f548575c54e94edb2d3fef4796636fb36
juliastetskaya <juliastetskaya@gmail.com>
2019-09-12 09:11:02 +0300

    add styles for spinner

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const Spinner = () => (
-  <div>loading...</div>
+  <div className="lds-css ng-scope" style={{ width: '200px', height: '200px' }}>
+    <div className="lds-spin" style={{ width: '100%', height: '100%' }}>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+    </div>
+  </div>
 );
 
 export default Spinner;
